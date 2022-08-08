61dc995f5925c04861acbd7560692015b70318d6
VAOkolo <vincentaokolo@outlook.com>
2022-08-08 12:46:19 +0100

    socket additions

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 require('dotenv').config()
 
 const PORT = process.env.PORT || 8000
+
+const server = http.createServer(app);
 
 //db
 const mongoose = require('mongoose')
 const { seedDB } = require('./seeds')
 
-const app = express()
-
-app.use(cors())
+app.use(cors());
 app.use(express.json())
 
 const userRoutes = require('./routes/userRoutes')
@@ -29,7 +32,7 @@ mongoose.connect(process.env.MONG_URI)
     //     seedDB()
     // })
     .then(() => {
-        app.listen(PORT, () => {
+        server.listen(PORT, () => {
             console.log("Connected to db & listening at port " + PORT)
         })
     })
@@ -37,3 +40,38 @@ mongoose.connect(process.env.MONG_URI)
         console.log(error)
     })
 
+
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("user conneted", socket.id);
+
+  socket.on("send_message", (data) => {
+    socket.broadcast.emit("recieved_message", data, socket.id);
+    console.log(data);
+  });
+
+
+  socket.on("send_canvas", (data, room) => {
+    console.log("socket io room :", room);
+    console.log("canvas ..data..sending data");
+
+    socket.to(room).emit("recieved_canvas", data);
+    socket.to(room).emit("recieved_id", socket.id);
+  });
+
+  socket.on("refresh_canvas", (data, room) => {
+    socket.to(room).emit("refreshed_canvas", data);
+  });
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log("join room", data, "userid", socket.id);
+  });
+});
